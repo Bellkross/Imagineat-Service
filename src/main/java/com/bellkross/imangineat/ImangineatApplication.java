2827bdf821c557b2b8866811d415648c7d8c4ed2
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-05-31 23:21:39 +0300

    Init project
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

---
+++ src/main/java/com/bellkross/imangineat/ImangineatApplication.java
@@ -0,0 +1,13 @@
+package com.bellkross.imangineat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImangineatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImangineatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bellkross/imangineat/ImangineatApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bellkross.imangineat;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImangineatApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
