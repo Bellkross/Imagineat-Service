7d092225c23d6da50d5bb2fcc282d137155d9c5c
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-06-09 15:25:13 +0300

    Added menu item photo repository
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

---
+++ src/main/java/com/bellkross/imangineat/respository/MenuItemPhotoRepository.java
@@ -0,0 +1,7 @@
+package com.bellkross.imangineat.respository;
+
+import com.bellkross.imangineat.entity.MenuItemPhoto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MenuItemPhotoRepository extends JpaRepository<MenuItemPhoto, String> {
+}
