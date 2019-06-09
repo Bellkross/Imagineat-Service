89e84565ca0721bd705ae6181fca3f9c20d841ef
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-06-09 16:20:53 +0300

    Added restaurant tag repository
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

---
+++ src/main/java/com/bellkross/imangineat/respository/RestaurantTagRepository.java
@@ -0,0 +1,7 @@
+package com.bellkross.imangineat.respository;
+
+import com.bellkross.imangineat.entity.RestaurantTag;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RestaurantTagRepository extends JpaRepository<RestaurantTag, Long> {
+}
