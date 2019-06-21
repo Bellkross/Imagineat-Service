bbc806fa0d12ad854ff4476a62c387199312854d
Kyrylo Vasylenko <vasilenkowm@gmail.com>
2019-06-21 19:13:08 +0300

    Added file downloading
    Signed-off-by: Kyrylo Vasylenko <vasilenkowm@gmail.com>

--- src/main/java/com/bellkross/imangineat/ImangineatApplication.java
+++ src/main/java/com/bellkross/imangineat/ImangineatApplication.java
@@ -1,9 +1,14 @@
 package com.bellkross.imangineat;
 
+import com.bellkross.imangineat.properties.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties({
+        FileStorageProperties.class
+})
 public class ImangineatApplication {
 
     public static void main(String[] args) {
